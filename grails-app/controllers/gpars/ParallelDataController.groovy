a0b881c6cb949ae12abe81457270f318731a2eeb
Vishal-Sahu <vishsahu@gmail.com>
2016-06-03 15:43:17 +0530

    Updated files

--- grails-app/controllers/gpars/ParallelDataController.groovy
+++ grails-app/controllers/gpars/ParallelDataController.groovy
@@ -3,18 +3,22 @@ package gpars
 import static groovyx.gpars.GParsPool.withPool
 
 class ParallelDataController {
-def parallelDataService
+    def parallelDataService
+
     def index() {
-        List<Integer> list=parallelDataService.populateList(100000)
-        List<Integer> list2=[]
-        Long startTime=System.nanoTime()
-        withPool(4){
-            list2=list.collectParallel{it*2}
+        List<Integer> list = parallelDataService.populateList(1000000)
+        List<Integer> list2 = []
+        Long startTime = System.currentTimeMillis()
+
+//        list2 = list.collect { it * 2 }
+
+        withPool(4) {
+            list2 = list.collectParallel { it * 2 }
         }
-        Long endTime=System.nanoTime()
-        Double millis=10**(-6)
-        Double totalTimeTaken=(endTime - startTime)*millis
-        println "TOTAL TIME :- ${totalTimeTaken} ms "
-        render "Total Time taken : ${totalTimeTaken} ms"
+        Long endTime = System.currentTimeMillis()
+
+        Double totalTimeTaken = (endTime - startTime) / 1000
+        println "*********** TOTAL TIME :- ${totalTimeTaken} Seconds "
+        render "*** Total Time taken : ${totalTimeTaken} Seconds"
     }
 }
--- grails-app/services/gpars/ParallelDataService.groovy
+++ grails-app/services/gpars/ParallelDataService.groovy
@@ -5,10 +5,10 @@ import grails.transaction.Transactional
 @Transactional
 class ParallelDataService {
 
-    List<Integer> populateList(Integer size){
-        List<Integer> populatedList=[]
-        if(size>0){
-            size.times{
+    List<Integer> populateList(Integer size) {
+        List<Integer> populatedList = []
+        if (size > 0) {
+            size.times {
                 populatedList << it
             }
         }
