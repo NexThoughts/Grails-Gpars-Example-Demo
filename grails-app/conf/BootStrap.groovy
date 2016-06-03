a7463ff410cbb71cfe1f444f1460bfd1afc5cc16
Vishal-Sahu <vishsahu@gmail.com>
2016-06-03 19:44:51 +0530

    Updated bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,16 @@
+import com.gpars.Person
+import groovyx.gpars.GParsPool
+
 class BootStrap {
 
+    def personService
+
     def init = { servletContext ->
+        GParsPool.withPool(15) {
+            (1..5000).eachParallel {
+                Person person = personService.createPerson("Mr Person ${it}", it * 5, 5)
+            }
+        }
     }
     def destroy = {
     }
