e3302722331dfcde2ef0afbc5960b50ad02367c2
Vishal-Sahu <vishsahu@gmail.com>
2016-06-03 16:29:48 +0530

    Working on gpars demo

--- grails-app/controllers/gpars/ParallelDataController.groovy
+++ grails-app/controllers/gpars/ParallelDataController.groovy
@@ -6,9 +6,14 @@ class ParallelDataController {
     def parallelDataService
 
     def index() {
+        Long startTimeList = System.currentTimeMillis()
+
         List<Integer> list = parallelDataService.populateList(1000000)
+
         List<Integer> list2 = []
         Long startTime = System.currentTimeMillis()
+        Double totalTimeTakenForData = (startTime - startTimeList) / 1000
+        render "*** Total Time taken for List Creation : ${totalTimeTakenForData} Seconds"
 
 //        list2 = list.collect { it * 2 }
 
